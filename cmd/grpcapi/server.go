bb6c0fd66d53ed4e22a02b049971201b535b427c
Sandwichzzy <62085707+Sandwichzzy@users.noreply.github.com>
2026-03-10 18:46:46 +0800

    dev:addteachers()

--- cmd/grpcapi/server.go
+++ cmd/grpcapi/server.go
@@ -1,1 +1,47 @@
-package grpcapi
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/Sandwichzzy/school_manager_system_grpc/internals/api/handlers"
+	"github.com/Sandwichzzy/school_manager_system_grpc/internals/repositories/mongodb"
+	pb "github.com/Sandwichzzy/school_manager_system_grpc/proto/gen"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// 初始化 MongoDB 客户端
+	mongodb.CreateMongoClient()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	s := grpc.NewServer()
+
+	pb.RegisterExecsServiceServer(s, &handlers.Server{})
+	pb.RegisterStudentsServiceServer(s, &handlers.Server{})
+	pb.RegisterTeachersServiceServer(s, &handlers.Server{})
+
+	reflection.Register(s)
+	// 监听端口，启动服务器等逻辑
+	port := os.Getenv("SERVER_PORT")
+
+	fmt.Println("grpc server running on port:", port)
+	// 监听端口
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// 启动 gRPC 服务器
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
